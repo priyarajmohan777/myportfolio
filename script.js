6eeb471d572e98f01b83ab45bdb88ca8d6edec39
priyarajmohan777 <priyarajmohan1111@gmail.com>
2025-03-27 13:29:08 +0530

    changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
